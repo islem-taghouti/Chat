df4faaf5a5731d691451c7f014853cf131449390
islem-taghouti <islemtaghouti01@gmail.com>
2023-07-23 14:29:26 +0100

    back end

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongosse = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const path = require("path");
 const multer = require("multer");
 dotenv.config();
@@ -44,6 +46,8 @@ app.use(morgan("common"));
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/posts", postRoute)
+app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute)
 app.listen(8800, () => {
     console.log("backend server is running ")
 })
